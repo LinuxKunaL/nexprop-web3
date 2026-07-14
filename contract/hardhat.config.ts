9ce7e4409a149a311ec68bda9c9cee6d31df66fd
LinuxKunaL <kunallokhande774@gmail.com>
2026-07-14 16:35:15 +0530

    feat(sm): add network in hardhat config

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,10 +1,21 @@
+import hardhatEthersPlugin from "@nomicfoundation/hardhat-ethers";
 import { defineConfig } from "hardhat/config";
 
 export default defineConfig({
   solidity: {
     version: "0.8.34",
   },
-  paths:{
-    sources:"./src"
-  }
+  paths: {
+    sources: "./src",
+  },
+  plugins: [hardhatEthersPlugin],
+  networks: {
+    localhost: {
+      type: "http",
+      url: "http://127.0.0.1:8545",
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
+    },
+  },
 });
