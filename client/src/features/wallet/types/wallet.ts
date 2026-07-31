83762a0a8509ee522e98800e424a448d80782efb
LinuxKunaL <kunallokhande774@gmail.com>
2026-07-31 23:16:04 +0530

    feat(wallet): extend TWalletConnection and TWalletMethods types

--- client/src/features/wallet/types/wallet.ts
+++ client/src/features/wallet/types/wallet.ts
@@ -12,4 +12,22 @@ export type TWalletConnection = {
   topic: string;
   address: string;
   chainId: number;
+  nativeDeepLink:string;
 };
+
+export type TWalletMethods =
+  | "eth_accounts"
+  | "eth_requestAccounts"
+  | "eth_chainId"
+  | "eth_getBalance"
+  | "eth_getTransactionCount"
+  | "eth_call"
+  | "eth_estimateGas"
+  | "eth_gasPrice"
+  | "eth_feeHistory"
+  | "eth_sendTransaction"
+  | "personal_sign"
+  | "eth_signTypedData"
+  | "eth_signTypedData_v4"
+  | "wallet_switchEthereumChain"
+  | "wallet_addEthereumChain";
