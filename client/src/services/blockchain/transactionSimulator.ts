bca9c4ee623b95e59264eb7a3e357847c6d8b18d
LinuxKunaL <kunallokhande774@gmail.com>
2026-08-01 16:50:24 +0530

    feat(transaction): add transaction simulator for blockchain calls

---
+++ client/src/services/blockchain/transactionSimulator.ts
@@ -0,0 +1,39 @@
+import {
+  Interface,
+  JsonRpcProvider,
+  TransactionRequest,
+  CallExceptionError,
+} from "ethers";
+import errorsABI from "@abi/errors.json";
+import config from "@config";
+
+const errorsInterface = new Interface(errorsABI);
+
+const provider = new JsonRpcProvider(
+  config.blockchain.rpcUrl,
+  config.blockchain.chainId,
+);
+
+export const transactionSimulator = async (
+  tx: TransactionRequest,
+): Promise<{ success: boolean; error?: string }> => {
+  try {
+    await provider.call(tx);
+
+    return { success: true };
+  } catch (error) {
+    const errorName = decodeError(error as CallExceptionError);
+
+    return { success: false, error: errorName };
+  }
+};
+const decodeError = (error: CallExceptionError): string | undefined => {
+  if (error.data == "0x") {
+    return "Internal blockchain error";
+  }
+
+  if (error.data) {
+    const parsedError = errorsInterface.parseError(error.data);
+    return parsedError?.name;
+  }
+};
