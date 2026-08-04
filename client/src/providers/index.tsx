da343a29f9e3f3d8f4cc824f0a97c1f9bf9f63bb
LinuxKunaL <kunallokhande774@gmail.com>
2026-08-04 17:49:04 +0530

    refactor(ui): restructure provider hierarchy and add ToastProvider

--- client/src/providers/index.tsx
+++ client/src/providers/index.tsx
@@ -3,17 +3,25 @@ import FontsProvider from "@providers/FontsProvider";
 import StyleProvider from "@providers/StyleProvider";
 import ThemeProvider from "@providers/ThemeProvider";
 import { KeyboardProvider } from "react-native-keyboard-controller";
+import { ToastProvider } from "@components/toast";
+import WalletCheckProvder from "./WalletCheckProvder";
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
   return (
-    <AuthProvider>
-      <FontsProvider>
-        <KeyboardProvider>
-          <ThemeProvider>
-            <StyleProvider>{children}</StyleProvider>
-          </ThemeProvider>
-        </KeyboardProvider>
-      </FontsProvider>
-    </AuthProvider>
+    <FontsProvider>
+      <KeyboardProvider>
+        <ThemeProvider>
+          <StyleProvider>
+            <ToastProvider maxToasts={6} defaultPosition="top">
+              {/* <AuthProvider> */}
+                {/* <WalletCheckProvder> */}
+                {children}
+                {/* </WalletCheckProvder> */}
+              {/* </AuthProvider> */}
+            </ToastProvider>
+          </StyleProvider>
+        </ThemeProvider>
+      </KeyboardProvider>
+    </FontsProvider>
   );
 }
